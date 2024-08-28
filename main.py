f17e0e77173b6081ed01a6954c0f19c952f59fb9
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-08-28 20:24:56 +0100

    refactor: Move Main.py file, Update Dockerfile and module imports

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from .app import crud, models, schemas
+from .app.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 

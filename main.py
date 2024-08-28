cf1866a65c13c4321c6bd9ccb0b6d0eb47ab5b8c
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-08-28 20:44:36 +0100

    feat: Add static file serving and template rendering

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
@@ -7,6 +10,12 @@ from app.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 # Dependency
